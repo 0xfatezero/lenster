4e08dff17f74a49d75eb90cd994c795d9c45b4d3
kagamimoe <kagamimio@gmail.com>
2023-03-25 01:52:52 +0000

    fix : add common.js

---
+++ utils/common.js
@@ -0,0 +1,18 @@
+function getRandomElement(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+  
+function delayFn(delay) {
+    return new Promise(resolve => {
+        const randomDelay = Math.floor(Math.random() * 10) + 1;
+        setTimeout(() => {
+        resolve();
+        }, (delay + randomDelay) * 1000);
+    });
+}
+  
+
+export {
+    getUserProfiles
+}
